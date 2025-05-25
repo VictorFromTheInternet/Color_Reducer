f88adb1ae5cd634b960ad32ef3d397d647fe3c40
VictorFromTheInternet <vma0430@gmail.com>
2025-05-25 14:08:20 -0500

    basic color input

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,1 +1,63 @@
-console.log('hello world')
+// event listener for the numColors range input (append/remove color inputs)
+document.getElementById("num-colors").addEventListener("input", ()=>{
+    numColorInputs = document.querySelectorAll(".color-input").length
+    numColors = Number(document.getElementById("num-colors").value)
+
+    if(numColors > numColorInputs)
+        appendColorInputs(numColors)
+    else if(numColors < numColorInputs)
+        removeColorInputs(numColors)
+})
+function appendColorInputs(numColors){
+    let colorInputsList = document.querySelectorAll(".color-input")
+    let currLen = colorInputsList.length
+    console.log(colorInputsList)
+    for(let i=currLen; i<numColors; i++){
+        let tempElem = document.createElement("input");
+        tempElem.type = "color"
+        tempElem.classList.add("form-control")
+        tempElem.classList.add("form-control-color")
+        tempElem.classList.add("color-input")
+        tempElem.id = `colorInput${i}`
+        document.getElementById("color-inputs-container").append(tempElem);
+        //colorInputsArr.append(colorInput)
+    }
+}
+function removeColorInputs(numColors){
+
+    // grab curr length and then iterate backwards
+    let colorInputsList = document.querySelectorAll(".color-input")
+    let currLen = colorInputsList.length
+
+    for(let i=currLen; i>numColors; i--){        
+        let tempId = `colorInput${i-1}`
+        //console.log(tempId)
+        //console.log(document.getElementById(`colorInput${i-1}`))
+
+
+        let tempElem = document.getElementById(tempId)
+        tempElem.remove()        
+    }
+}
+
+// collect color inputs and push to array
+function getColorInputValues(){
+    // query selector, all 
+    let tempArray = new Array()
+    let colorInputsList = document.querySelectorAll('.color-input')
+    //console.log(colorInputsList)
+
+    for(let i=0; i<colorInputsList.length; i++){        
+        let tempId = `colorInput${i}`
+        // console.log(tempId)
+        // console.log(document.getElementById(tempId))
+        // console.log(document.getElementById(tempId).value)
+
+        let tempValue = document.getElementById(tempId).value
+        tempValue = hexToRgb(tempValue)
+        tempArray.push(tempValue) 
+    }
+
+    //console.log(tempArray)
+    return tempArray
+}
